4d8ee5b07d1d202121b1087b30f7c3a847013e51
pz <pz@eszconsulting.com>
2017-10-16 14:10:10 +0100

    Added workaroudn for safe area inset behaviour added in iOS 11

--- src/ios/HackMainViewController.m
+++ src/ios/HackMainViewController.m
@@ -16,6 +16,11 @@ void emptyFunction(id self, SEL _cmd) {}
     self.webView.backgroundColor = [UIColor colorWithRed:36.0/255 green:36.0/255 blue:36.0/255 alpha:1.0];
     self.webView.opaque = NO;
      [UIApplication sharedApplication].applicationSupportsShakeToEdit = NO;
+    if ([self respondsToSelector:NSSelectorFromString(@"additionalSafeAreaInsets")]) {
+        if (@available(iOS 11.0, *)) {
+            self.additionalSafeAreaInsets = UIEdgeInsetsMake(-20, 0, 0, 0);
+        }
+    }
 }
 
 - (void) viewDidAppear:(BOOL)animated {
